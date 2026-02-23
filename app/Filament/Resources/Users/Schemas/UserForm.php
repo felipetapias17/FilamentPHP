5ea45e844d1aed851249bf2392fba214dc565b5c
felipetapias17 <tapiasfelipe604@gmail.com>
2026-02-23 00:17:35 -0500

    feat: Info country, state, city
    Informacion sobre pais, estado y ciudad

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,10 +2,15 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use App\Models\State;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\FormsComponent;
+use Illuminate\Support\Collection;
 use Filament\Schemas\Schema;
 use Filament\Schemas\Components\Section;
+
 
 class UserForm
 {
@@ -30,6 +35,33 @@ class UserForm
                 ])
                 ->columns(2)
                 ->columnSpanFull(),
+
+                Section::make('Address Info')
+                ->inlineLabel()
+                ->schema([
+                    Select::make('country_id')
+                    ->relationship('country', titleAttribute:'name')
+                    ->searchable()
+                    ->preload()
+                    ->live()
+                    ->required(),
+                    Select::make('state_id')
+                    ->options(fn(callable $get)=> 
+                    \App\Models\State::where('country_id', $get('country_id'))
+                    ->pluck('name', 'id'))
+                    ->searchable()
+                    ->preload()
+                    ->required(),    
+                    Select::make('city_id')
+                    ->options(fn(callable $get)=> 
+                    \App\Models\City::where('state_id', $get('state_id'))
+                    ->pluck('name', 'id'))
+                    ->searchable()
+                    ->preload()
+                    ->live()
+                    ->required()
+                ])
+
             ]);
     }
 }
